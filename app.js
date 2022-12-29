85474d8783d986737d0acdd0a2975b555146dfef
Maciej Muter <maciejmuter@gmail.com>
2022-12-29 12:09:52 +0100

    js with eval function

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+let calculations = [];
+let joinedCalc;
+
+function calculate(button) {
+  const value = button.textContent;
+
+  if (value === "C") {
+    calculations = [];
+    display.textContent = "0";
+  } else if (value === "=") {
+    display.textContent = eval(joinedCalc);
+    calculations = [display.textContent];
+  } else if (calculations.length < 20) {
+    calculations.push(value);
+    joinedCalc = calculations.join("");
+    display.textContent = joinedCalc;
+  }
+}
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
