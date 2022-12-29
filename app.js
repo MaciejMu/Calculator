4fa5c1224047dea71d8d8b991fe87e215c9aba9c
Maciej Muter <maciejmuter@gmail.com>
2022-12-29 20:12:43 +0100

    js without eval

--- app.js
+++ app.js
@@ -1,25 +1,63 @@
-const display = document.querySelector(".display");
-const buttons = document.querySelectorAll("button");
+const currentNum = document.querySelector(".current");
+const previousNum = document.querySelector(".previous");
+const numbersBtns = document.querySelectorAll(".btn_num");
+const operatorBtns = document.querySelectorAll(".btn_operator");
+const equalBtn = document.querySelector(".btn_equal");
+const clearBtn = document.querySelector(".btn_C");
 
-let calculations = [];
-let joinedCalc;
+let result = "";
+let operator = "";
 
-function calculate(button) {
-  const value = button.textContent;
+///Listeners
+numbersBtns.forEach((button) => button.addEventListener("click", dislpayNum));
+operatorBtns.forEach((button) => button.addEventListener("click", operate));
 
-  if (value === "C") {
-    calculations = [];
-    display.textContent = "0";
-  } else if (value === "=") {
-    display.textContent = eval(joinedCalc);
-    calculations = [display.textContent];
-  } else if (calculations.length < 20) {
-    calculations.push(value);
-    joinedCalc = calculations.join("");
-    display.textContent = joinedCalc;
-  }
+equalBtn.addEventListener("click", showResult);
+clearBtn.addEventListener("click", cleanScreen);
+
+///Functions
+function dislpayNum() {
+  if (this.textContent === "." && currentNum.innerHTML.includes(".")) return;
+  if (this.textContent === "." && currentNum.innerHTML === "")
+    return (currentNum.innerHTML = "0.");
+  if (currentNum.innerHTML.length < 15)
+    currentNum.innerHTML += this.textContent;
 }
 
-buttons.forEach((button) =>
-  button.addEventListener("click", () => calculate(button))
-);
+function operate() {
+  if (this.textContent === "-" && currentNum.innerHTML === "") {
+    return (currentNum.innerHTML = "-");
+  } else if (currentNum.innerHTML === "") return;
+  if (previousNum.innerHTML !== "") {
+    showResult();
+  }
+  previousNum.innerHTML = currentNum.innerHTML + this.textContent;
+  currentNum.innerHTML = "";
+  operator = this.textContent;
+}
+
+function showResult() {
+  if (previousNum.innerHTML === "") return;
+  const a = parseFloat(previousNum.innerHTML);
+  const b = parseFloat(currentNum.innerHTML);
+  if (isNaN(a) || isNaN(b)) return;
+
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+  }
+
+  currentNum.innerHTML = result;
+  previousNum.innerHTML = "";
+  operator = "";
+}
+
+function cleanScreen() {
+  previousNum.innerHTML = "";
+  currentNum.innerHTML = "";
+  operator = "";
+}
