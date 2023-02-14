5d8d7196e4980feecddd8ca40ec5d2df6cb09851
Maciej Muter <maciejmuter@gmail.com>
2023-02-14 11:08:30 +0100

    delete old files

--- dist/functions/clear-function.js
+++
@@ -1,9 +0,0 @@
-import { currentNum } from "../app";
-import { previousNum } from "../app";
-import { updateOperator } from "../app";
-export function clear() {
-    if (currentNum && previousNum) {
-        previousNum.innerHTML = currentNum.innerHTML = "";
-        updateOperator("");
-    }
-}
